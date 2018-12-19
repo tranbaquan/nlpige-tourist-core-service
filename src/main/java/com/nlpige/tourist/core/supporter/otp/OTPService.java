d14a4e2896614923a99238f30abe2e6b668e9e5d
Joseph Maria Nguyen Ngoc Lam Bao Truong <baotruong11a@gmail.com>
2018-12-19 13:29:39 +0700

    Merge OTP supporter package (#15)
    * 18/12.01
    
    * 19/12.01

---
+++ src/main/java/com/nlpige/tourist/core/supporter/otp/OTPService.java
@@ -0,0 +1,45 @@
+package com.nlpige.tourist.core.supporter.otp;
+
+import com.nlpige.tourist.exception.NLPigeException;
+import com.nlpige.tourist.exception.NotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+@Service
+public class OTPService {
+    @Autowired
+    private OTPRepository otpRepo;
+
+    public OTP saveOTP(OTP otp) {
+        otp.setExpireTime(LocalDateTime.now().plusMinutes(10));
+        return otpRepo.save(otp);
+    }
+
+    public OTP getOTP(String email) {
+        Optional<OTP> otp =otpRepo.findById(email);
+
+        if(!otp.isPresent()) {
+            throw new NotFoundException();
+        }
+        return otp.get();
+    }
+
+    private void deleteOTP(OTP otp) {
+        otpRepo.deleteById(otp.getEmail());
+    }
+
+    public boolean isCorrectOTP(OTP otp) {
+        OTP system = getOTP(otp.getEmail());
+        return validateOTP(system, otp.getOtp());
+    }
+
+    private boolean validateOTP(OTP systemOtp, String userOtp) {
+        if(systemOtp == null) {
+            throw new NLPigeException();
+        }
+        return systemOtp.getExpireTime().isAfter(LocalDateTime.now()) && systemOtp.getOtp().equals(userOtp) && systemOtp.getIdentifier().equals(userOtp);
+    }
+}
