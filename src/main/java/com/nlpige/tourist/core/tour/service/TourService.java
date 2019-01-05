edb469dab2711806decb9490234f9e8dd8923c04
Joseph Maria <baotruong11a@gmail.com>
2019-01-05 18:34:54 +0700

    Merge code
    Signed-off-by: Joseph Maria <baotruong11a@gmail.com>

--- src/main/java/com/nlpige/tourist/core/tour/service/TourService.java
+++ src/main/java/com/nlpige/tourist/core/tour/service/TourService.java
@@ -3,12 +3,15 @@ package com.nlpige.tourist.core.tour.service;
 import com.nlpige.tourist.core.collaborator.model.Collaborator;
 import com.nlpige.tourist.core.collaborator.service.CollaboratorService;
 import com.nlpige.tourist.core.tour.model.Tour;
+import com.nlpige.tourist.core.tour.model.TourRegisteringEntity;
 import com.nlpige.tourist.exception.CannotDeleteTour;
 import com.nlpige.tourist.exception.CollaboratorExistence;
 import com.nlpige.tourist.exception.NLPigeException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -19,7 +22,10 @@ public class TourService {
     @Autowired
     CollaboratorService collaboratorService;
     @Autowired
+    TourRegisteringRepository tourRegisteringRepository;
+    @Autowired
     PlaceRepository placeRepo;
+
     public Tour getTour(String tourId) {
         Optional<Tour> tour = tourRepo.findById(tourId);
         if (!tour.isPresent()) {
@@ -93,11 +99,6 @@ public class TourService {
             collaborators.add(tourRegisteringEntity.getCollaborator());
         }
         return collaborators;
-    }
-    public TourRegisteringEntity registerTour(String tourId, String collaboratorEmail) {
-        TourRegisteringEntity result = new TourRegisteringEntity(getTour(tourId), collaboratorEmail);
-        tourRegisteringRepository.save(result);
-        return result;
     }
 
 }
